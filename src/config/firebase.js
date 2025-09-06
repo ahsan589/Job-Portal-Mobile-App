a28c100d8d0c2465f430f7a1a6e5c84a81553491
Ahsan Hajvari <ahsam72642@gmail.com>
2025-09-06 23:44:03 +0500

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -6,15 +6,14 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAuI0yO4zSmRxElZxvFqLLpU6e3npC5Bho",
-  authDomain: "jobportal-8686b.firebaseapp.com",
-  databaseURL: "https://jobportal-8686b-default-rtdb.firebaseio.com",
-  projectId: "jobportal-8686b",
-  storageBucket: "jobportal-8686b.firebasestorage.app",
-  messagingSenderId: "213721520714",
-  appId: "1:213721520714:web:18f2c5a2b7285f61dae4b3",
-  measurementId: "G-N31HPGG704"
+  apiKey: "your-api-key",
+  authDomain: "your-project.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "your-app-id"
 };
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
